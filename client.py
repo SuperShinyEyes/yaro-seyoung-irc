debf37457a9f7f9170b30608b4f02af899198864
YOUNG <young@YOUNGs-MacBook-Pro.local>
2017-03-10 15:09:57 +0200

    Write partial client

--- client.py
+++ client.py
@@ -7,26 +7,27 @@ import sys  #for exit
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 except socket.error:
-    print 'Failed to create socket'
+    print('Failed to create socket')
     sys.exit()
 
-print 'Socket Created'
+print('Socket Created')
 
-host = 'www.google.com';
-port = 80;
-
-try:
-    remote_ip = socket.gethostbyname( host )
-
-except socket.gaierror:
-    #could not resolve
-    print 'Hostname could not be resolved. Exiting'
-    sys.exit()
+host = 'Aalto';
+port = 8888;
+# remote_ip = "10.100.57.138"
+remote_ip = "localhost"
+# try:
+#     remote_ip = socket.gethostbyname( host )
+#
+# except socket.gaierror:
+#     #could not resolve
+#     print 'Hostname could not be resolved. Exiting'
+#     sys.exit()
 
 #Connect to remote server
 s.connect((remote_ip , port))
 
-print 'Socket Connected to ' + host + ' on ip ' + remote_ip
+print('Socket Connected to ' + host + ' on ip ' + remote_ip)
 
 #Send some data to remote server
 message = "GET / HTTP/1.1\r\n\r\n"
@@ -36,12 +37,23 @@ try :
     s.sendall(message)
 except socket.error:
     #Send failed
-    print 'Send failed'
+    print('Send failed')
     sys.exit()
 
-print 'Message send successfully'
+print('Message send successfully')
 
-#Now receive data
-reply = s.recv(4096)
+while True:
+    #Now receive data
+    try :
+        message = raw_input(">>> ")
+        #Set the whole string
+        s.sendall(message)
+    except socket.error:
+        #Send failed
+        print('Send failed')
+        sys.exit()
 
-print reply
+
+    reply = s.recv(4096)
+
+    print(reply)
--- server.py
+++ server.py
@@ -1,9 +1,15 @@
 import socket
 import sys
-from thread import *
+import thread
 
 HOST = ''   # Symbolic name meaning all available interfaces
 PORT = 8888 # Arbitrary non-privileged port
+
+class ConnectionLife():
+    def __init__(self):
+        self.dead = False
+
+connection_life = ConnectionLife()
 
 connections = [] # Store all connectioins.
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -23,12 +29,12 @@ s.listen(10)
 print 'Socket now listening'
 
 #Function for handling connections. This will be used to create threads
-def clientthread(conn):
+def clientthread(conn, life):
     #Sending message to connected client
     conn.send('Welcome to the server. Type something and hit enter\n') #send only takes string
 
     #infinite loop so that function do not terminate and thread do not end.
-    while True:
+    while not life.dead:
 
         #Receiving from client
         data = conn.recv(1024)
@@ -41,17 +47,32 @@ def clientthread(conn):
                 c.sendall(reply)
 
         # conn.sendall(reply)
-
+    print("#came out of loop")
     #came out of loop
     conn.close()
 
+def close_all(socket, connections):
+    # for c in connections:
+    #     print("close conn")
+    #     c.close()
+    # thread.interrupt_main()
+    socket.close()
+
 #now keep talking with the client
 while 1:
-    #wait to accept a connection - blocking call
-    conn, addr = s.accept()
-    print 'Connected with ' + addr[0] + ':' + str(addr[1])
-    connections.append(conn)
-    #start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
-    start_new_thread(clientthread ,(conn,))
+    try:
+        #wait to accept a connection - blocking call
+        conn, addr = s.accept()
+        print 'Connected with ' + addr[0] + ':' + str(addr[1])
+        connections.append(conn)
+        #start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
+        thread.start_new_thread(clientthread ,(conn,connection_life,))
+    except KeyboardInterrupt:
+        connection_life.dead = True
+        print("KeyboardInterrupt detected")
+        break
+    # finally:
+print("close socket")
 
-s.close()
+close_all(s, connections)
+# s.close()
